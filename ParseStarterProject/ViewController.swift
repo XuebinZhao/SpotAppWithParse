9b92fe5ccbaa97dbb0e6af11706b83f938516c55
XuebinZhao <xzhao00@citymail.cuny.edu>
2015-10-21 04:21:24 -0400

    simple registration for users

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -12,6 +12,66 @@ import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userName: UITextField!
+    
+    @IBOutlet var password: UITextField!
+    
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    func displayAlert(title: String, message: String) {
+        
+        var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func signUpButton(sender: AnyObject) {
+        
+        if userName.text == "" || password.text == "" {
+            // display an alert if any field is empty
+            displayAlert("Error in form", message: "Please enter a username and password")
+            
+        } else {
+            // set up a Spinner that for the registration is processing
+            activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
+            activityIndicator.center = self.view.center
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+            view.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+            
+            var errorMessage = "Please try again later"
+            
+            var user = PFUser()
+            user.username = userName.text
+            user.password = password.text
+            
+            user.signUpInBackgroundWithBlock({ (success, error) -> Void in
+                
+                self.activityIndicator.stopAnimating()
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                
+                if error == nil {
+                    // Signup successful
+                } else {
+                    if let errorString = error!.userInfo?["error"] as? String {
+                        errorMessage = errorString
+                    }
+                    
+                    self.displayAlert("Failed Signed", message: errorMessage)
+                }
+            })
+        }
+        
+    }
+    
+    @IBAction func logInButton(sender: AnyObject) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
