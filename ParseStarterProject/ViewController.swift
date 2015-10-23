0ed691223d1b3cd028ac10fdf55556428c84c0cd
simplymarcello <simplymarcello@gmail.com>
2015-10-22 20:23:55 -0400

    Removed the auto push code that made the app go straight to the user database.

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -117,12 +117,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    /*override func viewDidAppear(animated: Bool) {
-        if PFUser.currentUser() != nil {
-            self.performSegueWithIdentifier("login", sender: self)
-        }
-    }*/
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
